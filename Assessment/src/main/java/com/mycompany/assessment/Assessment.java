d2608684d601988341745d4036942e672731dbf0
Nompumelelo Hlongwane <61636340+Nompumelelo-np@users.noreply.github.com>
2024-02-16 14:37:03 +0200

    Add files via upload

---
+++ Assessment/src/main/java/com/mycompany/assessment/Assessment.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assessment;
+
+        
+/**
+ *
+ * @author Acer-User
+ */
+public class Assessment {
+    
+
+    public static void main(String[] args) {
+        
+     System.out.println("*");
+     System.out.println("**");
+     System.out.println("***");
+     System.out.println("*****");
+        
+        
+    }
+}
